9d5fcdd65c91a84ed130141f1424d437a26289a8
cait <User@Macintosh.local>
2014-07-29 10:09:51 -0700

    Non-switch specific code appears functional

--- parse_netplan.py
+++ parse_netplan.py
@@ -10,7 +10,7 @@
 #
 
 
-import sys,json
+import os,sys,json,re
 from array import *
 
 from arista_eos import arista_eos
@@ -29,10 +29,6 @@ def sw_protocol (p):
 	
 	
 np = dict()
-
-
-def np_switches ():
-	return np['switches'].keys()
 	
 def np_profiles (switch):
 	return np['switches'].get(switch)['profiles']
@@ -70,38 +66,15 @@ def np_vlan_weight (vlan):
 #
 # process a single switch in the network plan
 # The following files are created
-#		<switch-name>.cmd			Command file for this switch to configure replicast
+#		out/<switch-name>.cmd		Command file for this switch to configure replicast
 #									This file contains the fixed lines, and interface
 #									configurations for all pre-configured switchports
-#		<switch-name>.lldp			LLDP identification info:
+#		out/<switch-name>.lldp		LLDP identification info:
 #									1st line: macaddr that identifies the switch
 #									remaining lines:
 #										interface name and identifying lldp attribute
 # 
-def process_switch (np,switch_name,fixed_lines,profile_lines):
-	print "process_switch",switch_name,"fixed",fixed_lines,"profile",profile_lines
-	cmd_file = open (switch_name+".cmd",'w')
-	s = np['switches'].get(switch_name)
-	switchports_file = open (switch_name+".lldp",'w')
-	err = switchports_file.write(s['macaddr'])
-	if err != 0:
-		print "switchports file write err",err
-		exit(2)
-	p = s['protocol']
-	print "protocol",p
-	spc = switch_protocol(s['protocol'])
-	for line in fixed_lines:
-		cmd_file.write(line)
-	profiles = np_profile(switch_name)
-	for pname in profiles.keys():
-		lines = spc.apply_to_interface_list(profiles[pname],profile_lines[pname])
-		for line in lines:
-			cmd_file.write(line)
-#			write to switchport_file
 
-	switchports_file.close()
-	cmd_file.close()
-	return 0
 
 		
 if len(sys.argv) != 2:
@@ -111,30 +84,61 @@ if len(sys.argv) != 2:
 	
 netplan_js = open(sys.argv[1],'r').read()
 np = json.loads(netplan_js)
-switches = np_switches()
 
-fixed_lines=dict()
-profile_lines=dict()
+try:
+	os.mkdir("out")
+except:
+	pass
 
 for sp_name in np['protocols'].keys():
-	profile_lines[sp_name] = dict()
 	spc = sw_protocol(sp_name)
-	fixed_lines[sp_name] = spc.fixed_lines(spc,np)
 	for pname in np['profiles'].keys():
+		f = open ("out/"+sp_name+"."+pname+".cmd","w")
 		lines = spc.profile_lines(np,sp_name,pname)
-		profile_lines.get(sp_name)[pname] = lines
-		f = open (sp_name+"."+pname+".cmd","w")
-		for line in profile_lines.get(sp_name)[pname]:
-			f.write(line)
+		for line in lines:
+			f.write(line+"\n")
 		f.close()		
 
 
-for s in switches:
-	sp_name = np_protocol(s)
-	f = fixed_lines.get(sp_name)
-	p = profile_lines.get(sp_name)
-	process_switch(np,s,f,p)
+switchports_file = open ("out/lldp",'w')
+for switch_name in np['switches'].keys():
+	sp_name = np_protocol(switch_name)
+	spc = sw_protocol(sp_name)
+	fixed_lines = spc.fixed_lines(spc,np)
 	
+	cmd_file = open ("out/"+switch_name+".cmd",'w')
+		
+	for line in fixed_lines:
+		err = cmd_file.write(line+"\n")
+		if err:
+			print "cmdfile write error",err
+			exit(2)
+		
+	spc = sw_protocol(sp_name)
+	profiles = np_profile(switch_name)
+	protocol_name = np_protocol(switch_name)
+	for pname in profiles.keys():
+		plines = spc.profile_lines(np,sp_name,pname)
+		interfaces = np_switchports(switch_name,pname).split(',')
+		for interface in interfaces:
+			prs = np['protocols']
+			protocol = prs.get(protocol_name)
+			lldp_maps = protocol['lldp-map']
+			for map in lldp_maps:
+				m=re.match(map[0],interface)
+				if m:
+					subbed = "'" + map[1].replace('*',interface)+"'"
+					line = np_macaddr(switch_name)+" "+subbed+" --> "+interface+"\n"
+					switchports_file.write(line)
+					break
+		lines = spc.apply_to_interface_list(interfaces,plines)
+		for line in lines:
+			cmd_file.write(line+"\n")
+
+
+	cmd_file.close()
+
+switchports_file.close()	
 exit(0)
 
 
