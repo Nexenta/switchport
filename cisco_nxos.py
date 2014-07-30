c5d1774765164c42d14919224d972b39cdef29e5
cait <User@Macintosh.local>
2014-07-30 13:11:46 -0700

    First working version

--- cisco_nxos.py
+++ cisco_nxos.py
@@ -7,19 +7,78 @@ class cisco_nxos:
 	# Cisco NXOS requires queues be created globally
 	# MTU is assigned to queues
 	#
+	# First find the replicast % of the total bandwdith and its cos
+	# then it is mostly a static script with only a few variable fill-ins
+	#
 	def fixed_lines (self,sw_proto,np):
-		return []
+		rep_bw = 0
+		total_weight = 0
+		for vlan_name in np['vlans'].keys():
+			v = np['vlans'].get(vlan_name)			
+			if v['no-drop'] == 'true':
+				replicast_cos = v['cos']
+				rep_w = v['weight']
+			total_weight = total_weight + v['weight']
+		if rep_w == 0:
+			print "Error. Repicast bandwidth not assigned"
+			exit(4)
+		rep_percent = int(rep_w*100/total_weight)
+		def_percent = 100 - rep_percent
+
+		lines = []
+		lines.append('class-map type qos class-relicast')
+		lines.append('match cos '+str(replicast_cos))
+		lines.append('set qos-group 1')
+		lines.append('class class-default')
+		lines.append('set qos-group 0')
+		lines.append('system qos')
+		lines.append('service-policy type qos replicast-in-policy')
+		lines.append('class-map type queuing class-replicast')
+		lines.append('match qos-group 1')
+		lines.append('class-map type queing class-default')
+		lines.append('match qos-group 0')
+		lines.append('policy-map type queuing policy-bw')
+		lines.append('class type queuing  class-replicast')
+		lines.append('bandwidth percent '+str(rep_percent))
+		lines.append('class type queuing class-default')
+		lines.append('bandwidth percent '+str(def_percent))
+		lines.append('system qos')
+		lines.append('service-policy type queuing input policy-bw')
+		lines.append('service-policy type queuing output policy-bw')
+		lines.append('class-map type network-qos class-replicast')
+		lines.append('match qos-group 1')
+		lines.append('class-map clas-default')
+		lines.append('policy-map type network-qos replicast-default-nq-policy')
+		lines.append('class class-replicast')
+		lines.append('pause no-drop')
+		lines.append('mtu 9000')
+		lines.append('class class-default')
+		lines.append('mtu 9000') 
+		
+		return lines
 		
 	#
 	# return lines for the specified sp_name and profile_name
 	#
 	def profile_lines (self,np,sp_name,pname):
 		lines = []
-		lines.append("need to do something with "+pname+" for "+sp_name)
-		#
-		# for each vlan
-		#		...
-		#
+		profile=np['profiles'].get(pname)
+		n_vlans = len(profile)
+		
+		id = []
+		for vlan_name in profile:
+			id.append(str(np['vlans'].get(vlan_name)['vlan']))
+	
+		if n_vlans > 1:
+			lines.append('switchport mode trunk')
+			lines.append('switchport trunk allowed vlan '+','.join(id))
+		elif np['single-vlan-mode'] == 'access':
+			lines.append('switchport mode access')
+			lines.append('switchport access vlan '+id[0])
+		else:
+			lines.append('switchport mode trunk')
+			lines.append('switchport trunk allowed vlan '+id[0])
+				
 		return lines
 		
 	#
