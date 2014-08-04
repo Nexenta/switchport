7379c9b6539f71348ba0efe5c03a97972711ea37
cait <User@Macintosh.local>
2014-08-04 11:03:48 -0700

    Added pfc no-drop lines kk

--- inventec_icos.py
+++ inventec_icos.py
@@ -17,23 +17,40 @@ class inventec_icos:
 		n_vlans = len(profile)
 		
 		id = []
+		weight = []
+		total_weight = 0
+		no_drop_cos = 9
 		for vlan_name in profile:
 			id.append(str(np['vlans'].get(vlan_name)['vlan']))
-	
+			v = np['vlans'].get(vlan_name)
+			weight.append(v['weight'])
+			total_weight = total_weight + v['weight']
+			if v['no-drop'] == 'true':
+				no_drop_cos=v['cos']
+			
+		if no_drop_cos == 9:
+			lines.append("Error: no-drop must be set for exactly one vlan")
+		
 		if n_vlans > 1:
 			lines.append('vlan participation include  '+','.join(id))
 			lines.append('vlan acceptframe vlanonly')
-			lines.append('vlan ingressfilter')
+			#set bandwidth percents
 		elif np['single-vlan-mode'] == 'access':
 			lines.append('vlan participation include '+id[0])
 			lines.append('vlan pvid '+id[0])
+			lines.append('vlan priority '+str(no_drop_cos))
 			lines.append('vlan acceptframe admituntaggedonly')
-			lines.append('vlan ingressfilter')
 		else:
 			lines.append('vlan participation include  '+id[0])
 			lines.append('vlan acceptframe vlanonly')
-			lines.append('vlan ingressfilter')
-				
+
+		lines.append('vlan ingressfilter')	
+		#spanning tree	
+		lines.append('datacenter-bridging')
+		lines.append('priority-flow-control mode on')
+		lines.append('priority-flow-control priority '+str(no_drop_cos)+' no-drop')	
+		lines.append('exit')
+		lines.append('exit')	
 		return lines
 		
 	#
