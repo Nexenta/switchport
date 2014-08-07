d2e02f2fd426297e8fc8969df0b979ffc436f9da
cait <User@Macintosh.local>
2014-08-07 09:56:03 -0700

    Various syntax errors corrected. Shift service-policy to being applied on a per-interface basis. Create per-profile policies.

--- cisco_nxos.py
+++ cisco_nxos.py
@@ -18,49 +18,53 @@ class cisco_nxos:
 	# then it is mostly a static script with only a few variable fill-ins
 	#
 	def fixed_lines (self,sw_proto,np):
-		rep_bw = 0
-		total_weight = 0
+		lines = []
+		replicast_cos = 1000
 		for vlan_name in np['vlans'].keys():
-			v = np['vlans'].get(vlan_name)			
+			v = np['vlans'].get(vlan_name)
 			if v['no-drop'] == 'true':
 				replicast_cos = v['cos']
-				rep_w = v['weight']
-			total_weight = total_weight + v['weight']
-		if rep_w == 0:
-			print "Error. Repicast bandwidth not assigned"
+				break
+		if replicast_cos == 1000:
+			print "Error: A no-drop vlan must be defined"
 			exit(4)
-		rep_percent = int(rep_w*100/total_weight)
-		def_percent = 100 - rep_percent
-
-		lines = []
-		lines.append('class-map type qos class-relicast')
+		lines.append('class-map type qos class-replicast')
 		lines.append('match cos '+str(replicast_cos))
+		lines.append('exit')
+		lines.append('policy-map type qos replicast-in-policy')
+		lines.append('class class-replicast')
 		lines.append('set qos-group 1')
-		lines.append('class class-default')
-		lines.append('set qos-group 0')
-		lines.append('system qos')
-		lines.append('service-policy type qos replicast-in-policy')
+		lines.append('exit')
+		lines.append('exit')
 		lines.append('class-map type queuing class-replicast')
 		lines.append('match qos-group 1')
-		lines.append('class-map type queing class-default')
-		lines.append('match qos-group 0')
-		lines.append('policy-map type queuing policy-bw')
-		lines.append('class type queuing  class-replicast')
-		lines.append('bandwidth percent '+str(rep_percent))
-		lines.append('class type queuing class-default')
-		lines.append('bandwidth percent '+str(def_percent))
-		lines.append('system qos')
-		lines.append('service-policy type queuing input policy-bw')
-		lines.append('service-policy type queuing output policy-bw')
+		lines.append('exit')
 		lines.append('class-map type network-qos class-replicast')
 		lines.append('match qos-group 1')
-		lines.append('class-map clas-default')
-		lines.append('policy-map type network-qos replicast-default-nq-policy')
-		lines.append('class class-replicast')
-		lines.append('pause no-drop')
-		lines.append('mtu 9000')
-		lines.append('class class-default')
-		lines.append('mtu 9000') 
+		lines.append('exit')
+		for profile_name in np['profiles'].keys():
+			total_weight = 0
+			rep_weight = 0
+			replicast_cos
+			for vlan_name in np['profiles'].get(profile_name):
+				v = np['vlans'].get(vlan_name)
+				total_weight = total_weight + v['weight']
+				if v['no-drop'] == 'true':
+					rep_weight = v['weight']
+			rep_percent = int(rep_weight*100/total_weight)
+			def_percent = 100 - rep_percent
+			lines.append('policy-map type queuing replicast-policy-'+profile_name)
+			lines.append('class type queuing  class-replicast')
+			lines.append('bandwidth percent '+str(rep_percent))
+			lines.append('class type queuing class-default')
+			lines.append('bandwidth percent '+str(def_percent))
+			lines.append('exit')
+			lines.append('policy-map type network-qos replicast-netpolicy')
+			lines.append('class class-replicast')
+			lines.append('pause no-drop')
+			lines.append('mtu 9000')
+			lines.append('class class-default')
+			lines.append('mtu 9000') 
 		
 		return lines
 		
@@ -89,6 +93,10 @@ class cisco_nxos:
 			lines.append('switchport trunk allowed vlan '+id[0])
 			lines.append('spanning-tree port-type edge trunk')
 				
+		lines.append('service-policy input replicast-in-policy')
+		lines.append('service-policy type queuing input replicast-policy-'+pname)
+		lines.append('service-policy type queuing output replicast-policy-'+pname)
+		lines.append('service-policy type network-qos replicast-netpolicy')
 		return lines
 		
 	#
